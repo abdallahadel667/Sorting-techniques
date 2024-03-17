af8d186a5967a500f4cb60899b94e2426af22c6b
‪ebrahim alaa eldin ebrahim‬‏ <135566800+ebrahimAlaaeldin@users.noreply.github.com>
2024-03-17 23:04:55 +0200

    Refactor sorting algorithm selection and output format

--- Main.java
+++ Main.java
@@ -16,45 +16,20 @@ public class Main {
         ArrayList<ArrayList<Integer>> arrayListList = new ArrayList<>();
         long startTime, endTime;
 
-        try {
-            // read line from txt file
-            array = rf2.read();
-
-            // startTime = System.nanoTime();
-            // arr.sort(null);
-            // endTime=System.nanoTime();
-            // System.out.println("Time taken to sort the array: " + (endTime-startTime) +
-            // "ns");
-        } catch (IOException e) {
-            System.out.println("Error reading file: " + filePath);
-            e.printStackTrace();
-            sc.close();
-        }
         while (true) {
             System.out.println("Welcome to the sorting program");
             System.out.println("................................");
-            System.out.println(" Please select the type of output you want:");
-            System.out.println("1. Return the sorted array only");
-            System.out.println("2. Return the intermediate steps of the sorting algorithm");
-            System.out.println("3. Exit");
-            int choiceFormate = sc.nextInt();
-
-            if (choiceFormate == 1) {
-                System.out.println("You have selected to return the sorted array only");
-            } else if (choiceFormate == 2) {
-                System.out.println("You have selected to return the intermediate steps of the sorting algorithm");
-            }
-
-            else if (choiceFormate == 3) {
-                System.out.println("Exiting the program");
-                System.exit(0);
-            } else {
-                System.out.println("Invalid choice");
+            System.out.println("Please provide the path of the file containing the array to be sorted: " + filePath);
+            try {
+                // read line from txt file
+                array = rf2.read();
+            } catch (IOException e) {
+                System.out.println("Error reading file: " + filePath);
+                e.printStackTrace();
                 continue;
-
             }
-
-            sortArray(array, choiceFormate);// call the sortArray method
+            System.out.println("The array to be sorted is: " + array);
+            sortArray(array);
 
             array.sort(null);
             System.out.println(array);
@@ -66,8 +41,9 @@ public class Main {
 
     }
 
-    public static void sortArray(ArrayList<Integer> array, int choiceFormate) {
+    public static void sortArray(ArrayList<Integer> array) {
         Scanner sc = new Scanner(System.in);
+        int choiceFormate = ReturnFormate();
         System.out.println("Pleae select the sorting algorithm you want to use");
         System.out.println("1. Insertion Sort");
         System.out.println("2. Merge Sort");
@@ -75,14 +51,16 @@ public class Main {
         System.out.println("4. Return to main menu");
         System.out.println("5. Exit");
         int choice = sc.nextInt();
+
         switch (choice) {
             case 1:
+                System.out.println("You have selected Insertion Sort");
                 if (choiceFormate == 1) {
-                    System.out.println("You have selected Insertion Sort");
+
                     System.out.println("The sorted array is: ");
                     System.out.println(InsertionSort.sort(array, choiceFormate));
                 } else if (choiceFormate == 2) {
-                    System.out.println("You have selected Insertion Sort");
+
                     System.out.println("The intermediate steps of the sorting algorithm are: ");
                     InsertionSort.sort(array, choiceFormate);
                 } else {
@@ -90,12 +68,13 @@ public class Main {
                 }
                 break;
             case 2:
+                System.out.println("You have selected Merge Sort");
                 if (choiceFormate == 1) {
-                    System.out.println("You have selected Merge Sort");
+
                     System.out.println("The sorted array is: ");
                     System.out.println(MergeSort.sort(array, choiceFormate));
                 } else if (choiceFormate == 2) {
-                    System.out.println("You have selected Merge Sort");
+
                     System.out.println("The intermediate steps of the sorting algorithm are: ");
                     MergeSort.sort(array, choiceFormate);
                 } else {
@@ -103,12 +82,13 @@ public class Main {
                 }
                 break;
             case 3:
+                System.out.println("You have selected Counting Sort");
                 if (choiceFormate == 1) {
-                    System.out.println("You have selected Counting Sort");
+
                     System.out.println("The sorted array is: ");
                     System.out.println(CountingSort.sort(array, choiceFormate));
                 } else if (choiceFormate == 2) {
-                    System.out.println("You have selected Counting Sort");
+
                     System.out.println("The intermediate steps of the sorting algorithm are: ");
                     CountingSort.sort(array, choiceFormate);
                 } else {
@@ -128,4 +108,33 @@ public class Main {
         }
 
     }
+
+    public static int ReturnFormate() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+
+            System.out.println(" Please select the type of output you want:");
+            System.out.println("1. Return the sorted array only");
+            System.out.println("2. Return the intermediate steps of the sorting algorithm");
+            System.out.println("3. Exit");
+            int choiceFormate = sc.nextInt();
+
+            if (choiceFormate == 1) {
+                System.out.println("You have selected to return the sorted array only");
+                return 1;
+            } else if (choiceFormate == 2) {
+                System.out.println("You have selected to return the intermediate steps of the sorting algorithm");
+                return 2;
+            } else if (choiceFormate == 3) {
+                System.out.println("Exiting the program");
+                System.exit(0);
+            } else {
+                System.out.println("Invalid choice");
+                continue;
+
+            }
+        }
+
+    }
+
 }
