15bc6a95ff795e807a9f5a7e8709a32a28beac82
‪ebrahim alaa eldin ebrahim‬‏ <135566800+ebrahimAlaaeldin@users.noreply.github.com>
2024-03-18 00:36:20 +0200

    final edit for Counting sort

--- Main/src/CountingSort/CountingSort.java
+++ Main/src/CountingSort/CountingSort.java
@@ -6,6 +6,8 @@ import java.util.Collections;
 public class CountingSort {
     private ArrayList<Integer> list;
     private int size; 
+    private ArrayList<int[]> intermediateArrayList = new ArrayList<>();
+    private int[] finalArray= new int[size];
 
     public CountingSort(ArrayList<Integer> list) {
         this.list = list;
@@ -18,8 +20,8 @@ public class CountingSort {
         int max = Collections.max(list);
         // Initialize frequency array
         int[] freq_arr = new int[max + 1];
-        int[] ans = new int[size];
-        ArrayList<int[]> intermediateArrays = new ArrayList<>();
+        // int[] ans = new int[size];
+        // ArrayList<int[]> intermediateArrays = new ArrayList<>();
         // Counting the number of occurences
         for (int i = 0; i < size; i++) {
             freq_arr[list.get(i)]++;
@@ -34,15 +36,15 @@ public class CountingSort {
     ArrayList<int[]> returnIntermediateArrays() {
         int[] freq_arr = sort();
         int[] ans = new int[size];
-        ArrayList<int[]> intermediateArrays = new ArrayList<>();
+       
         for (int i = size - 1; i >= 0; i--) {
             int value = list.get(i);
             freq_arr[value]--;
             ans[freq_arr[value]] = value;
             int[] snapShot = ans.clone();
-            intermediateArrays.add(snapShot);
+            this.intermediateArrayList.add(snapShot);
         }
-        return intermediateArrays;
+        return intermediateArrayList;
     }
 
     int[] returnFinalSorted() {
@@ -55,4 +57,22 @@ public class CountingSort {
         }
         return ans;
     }
+    void printIntermediate() {
+        System.out.println("The intermediate steps of the sorting algorithm are: ");
+        for (int[] arr : intermediateArrayList) {
+            for (int i : arr) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    void printFinal() {
+        System.out.println("The sorted array is: ");
+        for (int i : finalArray) {
+            System.out.print(i + " ");
+        }
+    }
+
+
 }
